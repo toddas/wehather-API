f243b6010233649420ca9b26aa86867b1c9d0959
toddas <48078787+toddas@users.noreply.github.com>
2019-03-12 16:39:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import urllib.request
+import json
+
+api_url = "http://api.openweathermap.org/data/2.5/weather"
+city = input("Iveskite miesta: ")
+api_key = "9a80336d5c6fa54817f1da43cba71299"
+req_unit ="metric"
+
+url = api_url + "?q=" + city + "&appid=" + api_key +"&units="+ req_unit
+responce = urllib.request.urlopen(url)
+parse_data = json.loads(responce.read())
+temperatura = parse_data["main"]["temp"]
+oras = parse_data["weather"][0]["description"]
+icona = parse_data["weather"][0]["icon"]
+
+
+print(str(temperatura) + "C")
+print(oras)
+print(icona)
